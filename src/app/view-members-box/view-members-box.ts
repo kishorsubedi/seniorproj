4982dffc8e33be02c3e72fa7fa9fc9ba4f84211b
TeslaC-137 <sudarshan.praj@gmail.com>
2020-04-13 16:52:13 -0400

    Moving make admin and invite member boxes to viewmember bos

--- src/app/view-members-box/view-members-box.ts
+++ src/app/view-members-box/view-members-box.ts
@@ -3,6 +3,7 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/fire
 import { Observable } from 'rxjs';
 import { UsersService } from '../services/users-service';
 import { User } from '../models/user';
+import { AuthService } from '../services/auth.service'; 
 
 @Component({
   selector: 'app-view-members-box',
@@ -19,7 +20,7 @@ export class ViewMembersBox implements OnInit {
   searchText: string;
 
 
-  constructor(private usersService: UsersService) {  }
+  constructor(private usersService: UsersService, private auth: AuthService) {  }
  
   ngOnInit(){
     this.members = this.usersService.getMembers();
@@ -53,6 +54,62 @@ export class ViewMembersBox implements OnInit {
   updateDisplay(){
     this.membersInDisplay = this.searchInArray(this.members);
     this.adminsInDisplay = this.searchInArray(this.admins);
+  }
+
+  inviteMember(inviteEmail: string){
+
+    this.auth.afs.firestore.doc('/users/'+ inviteEmail).get()
+      .then(docSnapshot => {
+        if (docSnapshot.exists) {
+          // do something
+          window.alert("User already a member!");
+        }
+        else{
+          this.auth.afs.firestore.doc('/invitedMembers/'+ inviteEmail).get()
+          .then(docSnapshot => {
+            if (docSnapshot.exists) {
+              // do something
+              window.alert("this user is already invited!");
+            }
+            else{
+                var inviteMembersCollectionRef = this.auth.afs.collection('invitedMembers'); // a ref to the users collection
+                inviteMembersCollectionRef.doc(inviteEmail).set({ email: inviteEmail });
+                window.alert("User invited");
+            }
+          });
+        }
+      });
+
+  }
+
+  makeAdmin(makeAdminEmail: string)
+  {
+    //is admin ? yes window.alert("Already an admin");
+    //No, then if user-> make. no -> have to be an user.
+    this.auth.afs.firestore.doc('/admins/'+ makeAdminEmail).get()
+          .then(docSnapshot => {
+              if (docSnapshot.exists) {
+                // do something
+                window.alert("Already an admin!");
+              }
+              else{
+                    
+                  this.auth.afs.firestore.doc('/users/'+ makeAdminEmail).get()
+                .then(docSnapshot => {
+                    if (docSnapshot.exists) {
+                      // make adminn
+                      var adminsCollectionRef = this.auth.afs.collection('admins'); // a ref to the users collection
+                      adminsCollectionRef.doc(makeAdminEmail).set({ email: makeAdminEmail });
+                      
+                      this.auth.afs.firestore.doc(`users/${makeAdminEmail}`).delete();
+                    }
+                    else{
+                        window.alert("Have to be an user!");
+                    }
+                });
+
+              }
+          });
   }
 }
 
