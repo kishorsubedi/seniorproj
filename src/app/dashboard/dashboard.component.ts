469d2a3d306dc6f7005b02b7601437394c890f34
Anita  Calmday <anitacalmday@Anitas-MacBook-Pro.local>
2020-04-23 22:11:01 -0400

    dashbaord updated

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -14,9 +14,41 @@ export class DashboardComponent implements OnInit {
   constructor(private auth: AuthService, private router: Router) { 
     this.isAdmin();
   }
+  loadStripe() {
+     
+    if(!window.document.getElementById('stripe-script')) {
+      var s = window.document.createElement("script");
+      s.id = "stripe-script";
+      s.type = "text/javascript";
+      s.src = "https://checkout.stripe.com/checkout.js";
+      window.document.body.appendChild(s);
+    }
+}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loadStripe()
   }
+  pay(amount) {    
+ 
+    var handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_aeUUjYYcx4XNfKVW60pmHTtI',
+      locale: 'auto',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+        console.log(token)
+        alert('Token Created!!');
+      }
+    });
+ 
+    handler.open({
+      name: 'Pay Your ACM Dues',
+      description: 'thank you for your commitment!',
+      amount: amount * 100
+    });
+ 
+}
+
 
   async isAdmin(){
     // var currUid = this.auth.afAuth.auth.currentUser.uid;
